a7b57e4d58858a5631103ddef9e483b39b4520c7
Andrew Bell <andy.bell@displaynote.com>
2020-02-05 16:18:16 +0100

    Initial rust server app for Nebula

---
+++ Nebula/nebula-server/src/main.rs
@@ -0,0 +1,12 @@
+extern crate libc;
+//use libc::{c_int, size_t};
+//use std::time::{Instant};
+
+#[link(name = "Nebula", kind = "static")]
+
+extern {
+}
+
+fn main() {
+    println!("Hello, world!");
+}
