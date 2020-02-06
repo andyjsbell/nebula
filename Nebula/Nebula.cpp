76b1a54e6d23ac63728aa3058b081d81ebc67882
Andy Bell <andy.bell@displaynote.com>
2020-02-06 16:29:14 +0100

    Implement new functions

--- Nebula/Nebula.cpp
+++ Nebula/Nebula.cpp
@@ -1,14 +1,17 @@
 #include "Nebula.h"
-#include "DXGIManager.h"
 
 DXGIManager * _manager = nullptr;
 
 extern "C" bool CreateManager(int outWidth, int outHeight)
 {
 	if (_manager == nullptr)
+	{
+		CoInitialize(NULL);
 		_manager = new DXGIManager(outWidth, outHeight);
-
-	return true;
+	}
+	HRESULT hr = _manager->SetCaptureSource(CaptureSource::CSDesktop);
+	
+	return hr == S_OK;
 }
 
 extern "C" bool IsSupported()
@@ -16,6 +19,54 @@ extern "C" bool IsSupported()
 	if (_manager != nullptr)
 	{
 		return _manager->IsSupported() == S_OK;
+	}
+
+	return false;
+}
+
+extern "C" bool GetOutputRect(int& x, int&y, int& width, int& height)
+{
+	if (_manager != nullptr)
+	{
+		RECT r;
+		if (S_OK == _manager->GetOutputRect(r))
+		{
+			x = r.left;
+			y = r.top;
+			width = r.right - r.left;
+			height = r.bottom -  r.top;
+			
+			return true;
+		}
+	}
+
+	return false;
+}
+
+extern "C" bool GetOriginalRect(int& x, int&y, int& width, int& height)
+{
+	if (_manager != nullptr)
+	{
+		RECT r;
+		if (S_OK == _manager->GetOriginalRect(r))
+		{
+			x = r.left;
+			y = r.top;
+			width = r.right - r.left;
+			height = r.bottom - r.top;
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+extern "C" bool GetOutputBits(BYTE** pBits, DWORD& outLen, bool& nv12)
+{
+	if (_manager != nullptr)
+	{
+		return S_OK == _manager->GetOutputBits(pBits, outLen, nv12);
 	}
 
 	return false;
