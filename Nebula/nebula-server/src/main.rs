af3271deb85ec661b78643e61c250ab72e6edafc
Andy Bell <andy.bell@displaynote.com>
2020-02-06 12:49:32 +0100

    Set grab source Functions to get output and original rectangles

--- Nebula/nebula-server/src/main.rs
+++ Nebula/nebula-server/src/main.rs
@@ -6,6 +6,8 @@ extern crate libc;
 extern {
     fn CreateManager(outWidth: u32, outHeight: u32) -> bool;
     fn IsSupported() -> bool;
+    fn GetOutputRect(x: &u32, y: &u32, width: &u32, height: &u32) -> bool;
+    fn GetOriginalRect(x: &u32, y: &u32, width: &u32, height: &u32) -> bool;
 }
 
 fn create_manager(out_width: u32, out_height: u32) -> bool {
@@ -20,8 +22,39 @@ fn is_supported() -> bool {
     }
 }
 
+#[derive(Debug)]
+struct Rect(u32, u32, u32, u32);
+
+fn get_output_rect() -> Rect {
+    unsafe {
+        let x: u32 = 0;
+        let y: u32 = 0;
+        let width: u32 = 0;
+        let height: u32 = 0;
+
+        GetOutputRect(&x, &y, &width, &height);
+
+        Rect(x, y, width, height)
+    }
+}
+
+fn get_original_rect() -> Rect {
+    unsafe {
+        let x: u32 = 0;
+        let y: u32 = 0;
+        let width: u32 = 0;
+        let height: u32 = 0;
+
+        GetOriginalRect(&x, &y, &width, &height);
+
+        Rect(x, y, width, height)
+    }
+}
+
 fn main() {
     println!("Initialised Nebula C");
     println!("{}", create_manager(1920, 1080));
     println!("{}", is_supported());
+    println!("{:?}", get_output_rect());
+    println!("{:?}", get_original_rect());
 }
