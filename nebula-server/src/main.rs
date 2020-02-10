866b6c525e4fa12f1d61e48b4ebb5e8e938d648c
Andrew Bell <andy.bell@displaynote.com>
2020-02-10 16:22:26 +0100

    use bounded channel with 0 and receive frames in encoder thread

--- nebula-server/src/main.rs
+++ nebula-server/src/main.rs
@@ -1,13 +1,15 @@
 extern crate libc;
 #[macro_use]
 extern crate crossbeam_channel;
+extern crate timer;
+extern crate chrono;
 
 use libc::{c_int, size_t};
 use std::time::{Instant};
 use std::{ptr, thread};
 use std::net::TcpListener;
 use tungstenite::server::accept;
-use crossbeam_channel::unbounded;
+use crossbeam_channel::bounded;
 use std::sync::Arc;
 
 mod encoder;
@@ -17,21 +19,21 @@ fn main() {
     
     println!("Launching Nebula Server");
 
-    let (frame_sender, fr) = unbounded();
+    let (frame_sender, fr) = bounded(0);
     let fps: u32 = 60;
 
-    let frame_receiver = Arc::new(fr);  // Create a referenced count
+    // let frame_receiver = Arc::new(fr);  // Create a referenced count
 
     // Create shared frame for capture and encoding
-    let captured_frame: grabber::Frame = grabber::Frame::new(1920, 1080, true);
-    let mutex = std::sync::Mutex::new(captured_frame);
-    let mutex_arc = std::sync::Arc::new(mutex);
-    //
+    // let captured_frame: grabber::Frame = grabber::Frame::new(1920, 1080, true);
+    // let mutex = std::sync::Mutex::new(captured_frame);
+    // let mutex_arc = std::sync::Arc::new(mutex);
+    // //
 
     // Grabber scope
     let grabber;
     {   
-        let mutex_arc = mutex_arc.clone();
+        // let mutex_arc = mutex_arc.clone();
         grabber = move || {
         
             if !grabber::create_manager(1920, 1080) {
@@ -51,19 +53,19 @@ fn main() {
                 match grabber::get_output_bits() {
                     None => println!("Failed to get frame"),
                     Some(frame) => {
-                        println!("We have a frame nv12={0} width={1} height={2} size={3}",
+                        println!("Captured frame nv12={0} width={1} height={2} size={3}",
                                 frame.nv12,
                                 frame.width,
                                 frame.height,
                                 frame.data.len());
 
-                        let mut captured_frame = mutex_arc.lock().unwrap();
-                        captured_frame.data = frame.data;
-                        captured_frame.width = frame.width;
-                        captured_frame.height = frame.height;
-                        captured_frame.nv12 = frame.nv12;
+                        // let mut captured_frame = mutex_arc.lock().unwrap();
+                        // captured_frame.data = frame.data;
+                        // captured_frame.width = frame.width;
+                        // captured_frame.height = frame.height;
+                        // captured_frame.nv12 = frame.nv12;
                         
-                        // frame_sender.send(frame).unwrap();
+                        frame_sender.send(frame).unwrap();
                     }
                 }
 
@@ -84,12 +86,34 @@ fn main() {
         println!("Failed to initliase encoder");
     }
 
+    let encoder_thread;
+    {
+        // let mutex_arc = mutex_arc.clone();
+        encoder_thread = move || {
+            
+            loop {
+                // let mut captured_frame = mutex_arc.lock().unwrap();
+                let captured_frame = fr.recv().unwrap();
+                
+                println!("Encode frame nv12={0} width={1} height={2} size={3}",
+                        captured_frame.nv12,
+                        captured_frame.width,
+                        captured_frame.height,
+                        captured_frame.data.len());
+                
+                // thread::sleep_ms(1000 / fps);
+            }
+        };
+    }
+
+    thread::spawn(encoder_thread);
+
     // Create server and block on connections which are spawned into own thread
     let server = TcpListener::bind("127.0.0.1:9001").unwrap();
     
     for stream in server.incoming() {
         
-        let frame_receiver = Arc::clone(&frame_receiver);
+        // let frame_receiver = Arc::clone(&frame_receiver);
         
         thread::spawn (move || {
             
@@ -102,8 +126,8 @@ fn main() {
                     // If cmd 'f'
                     // Grab latest frame from channel
                     // Encode and send back in this thread
-                    let frames: Vec<grabber::Frame> = frame_receiver.iter().collect();
-                    let frame = frames.last();
+                    // let frames: Vec<grabber::Frame> = frame_receiver.iter().collect();
+                    // let frame = frames.last();
 
                 }
             }
