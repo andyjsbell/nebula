7cc6c5e2cdc56d447e413bfbbddc0dfdaf33f07b
Andrew Bell <andy.bell@displaynote.com>
2020-02-12 14:41:05 +0100

    Tidy

--- nebula-server/src/encoder.rs
+++ nebula-server/src/encoder.rs
@@ -2,34 +2,36 @@ extern crate ffmpeg4_ffi;
 extern crate num_cpus;
 
 use std::ffi::{CString, CStr};
-use std::{ptr, thread};
+use std::{ptr};
+use super::grabber::Frame;
 
 use ffmpeg4_ffi::sys::{
     self,
-    AVFrame,
+    // AVFrame,
     AVDictionary,
     AVCodec,
     AVCodecContext,
-    AVStream,
-    AVPacket,
-    AVFormatContext,
-    AVOutputFormat,
-    AVCodecParameters,
-    AVCodecParserContext,
-    AVMediaType,
-    AVMediaType_AVMEDIA_TYPE_UNKNOWN as AVMEDIA_TYPE_UNKNOWN,
-    AVMediaType_AVMEDIA_TYPE_VIDEO as AVMEDIA_TYPE_VIDEO,
-    AVMediaType_AVMEDIA_TYPE_AUDIO as AVMEDIA_TYPE_AUDIO,
-    AVMediaType_AVMEDIA_TYPE_DATA as AVMEDIA_TYPE_DATA,
-    AVMediaType_AVMEDIA_TYPE_SUBTITLE as AVMEDIA_TYPE_SUBTITLE,
-    AVMediaType_AVMEDIA_TYPE_ATTACHMENT as AVMEDIA_TYPE_ATTACHMENT,
-    AVMediaType_AVMEDIA_TYPE_NB as AVMEDIA_TYPE_NB,
-    AVFMT_NOFILE,
-    AVIO_FLAG_WRITE,
-    AVRounding_AV_ROUND_NEAR_INF as AV_ROUND_NEAR_INF,
-    AVRounding_AV_ROUND_PASS_MINMAX as AV_ROUND_PASS_MINMAX,
-    AVCodecID_AV_CODEC_ID_H264 as AV_CODEC_ID_H264,
-    AV_INPUT_BUFFER_PADDING_SIZE,
+    SwsContext,
+    // AVStream,
+    // AVPacket,
+    // AVFormatContext,
+    // AVOutputFormat,
+    // AVCodecParameters,
+    // AVCodecParserContext,
+    // AVMediaType,
+    // AVMediaType_AVMEDIA_TYPE_UNKNOWN as AVMEDIA_TYPE_UNKNOWN,
+    // AVMediaType_AVMEDIA_TYPE_VIDEO as AVMEDIA_TYPE_VIDEO,
+    // AVMediaType_AVMEDIA_TYPE_AUDIO as AVMEDIA_TYPE_AUDIO,
+    // AVMediaType_AVMEDIA_TYPE_DATA as AVMEDIA_TYPE_DATA,
+    // AVMediaType_AVMEDIA_TYPE_SUBTITLE as AVMEDIA_TYPE_SUBTITLE,
+    // AVMediaType_AVMEDIA_TYPE_ATTACHMENT as AVMEDIA_TYPE_ATTACHMENT,
+    // AVMediaType_AVMEDIA_TYPE_NB as AVMEDIA_TYPE_NB,
+    // AVFMT_NOFILE,
+    // AVIO_FLAG_WRITE,
+    // AVRounding_AV_ROUND_NEAR_INF as AV_ROUND_NEAR_INF,
+    // AVRounding_AV_ROUND_PASS_MINMAX as AV_ROUND_PASS_MINMAX,
+    // AVCodecID_AV_CODEC_ID_H264 as AV_CODEC_ID_H264,
+    // AV_INPUT_BUFFER_PADDING_SIZE,
 };
 
 fn number_of_threads(width: u32, height: u32, number_of_cores:u8) -> i32 {
@@ -128,18 +130,108 @@ impl Encoder {
 
             let opened = sys::avcodec_open2(self.codec_context, self.codec, &mut codec_options);
             
-            if opened < 0 {
-                println!("Failed to open codec ;-(");
-            } else {
-                println!("Opened codec :-)");
-            }
+            self.initialised = opened >= 0;
         }
         
-        return true;
+        return self.initialised;
     }
 
-    pub fn encode_frame(&mut self) {
+    pub fn encode_frame(&mut self, frame: Frame) -> Result<EncodedFrame, EncodeError> {
+        if !self.initialised {
+            if !self.initialise() {
+                return Err(EncodeError::Initialised);
+            }
+        }
 
+        unsafe {
+            let in_frame = sys::av_frame_alloc();
+            (*in_frame).format = (*self.codec_context).pix_fmt;
+
+            match frame.format {
+                sys::AVPixelFormat_AV_PIX_FMT_NV12 => {
+                    (*in_frame).width = frame.resolution.0 as i32;
+                    (*in_frame).height = frame.resolution.1 as i32;
+
+                    sys::avpicture_fill(in_frame as *mut sys::AVPicture, 
+                                        frame.data.as_ptr(), 
+                                        (*self.codec_context).pix_fmt, 
+                                        (*in_frame).width, (*in_frame).height);
+                },
+                sys::AVPixelFormat_AV_PIX_FMT_BGRA => {
+                    (*in_frame).width = frame.resolution.0 as i32;
+                    (*in_frame).height = frame.resolution.1 as i32;
+                    sys::av_image_alloc((*in_frame).data.as_mut_ptr(), 
+                                        (*in_frame).linesize.as_mut_ptr(), 
+                                        (*in_frame).width, 
+                                        (*in_frame).height, 
+                                        (*self.codec_context).pix_fmt, 
+                                        32);
+				
+                    (*in_frame).pts = self.pts;
+                    self.pts = self.pts + 1;
+
+                    self.sws_context = sys::sws_getCachedContext(self.sws_context,
+                        frame.screen_resolution.0 as i32,
+                        frame.screen_resolution.1 as i32,
+                        frame.format /* AV_PIX_FMT_BGRA */,
+                        (*self.codec_context).width,
+                        (*self.codec_context).height,
+                        (*in_frame).format /* AV_PIX_FMT_YUV420P */,
+                        0, 
+                        std::ptr::null_mut(), 
+                        std::ptr::null_mut(), 
+                        std::ptr::null_mut());
+                    
+                    let src_stride:[i32; 1] = [frame.screen_resolution.0 as i32 * 4];
+                    
+                    sys::sws_scale( self.sws_context, 
+                                    &(frame.data.as_ptr()), 
+                                    src_stride.as_ptr(), 
+                                    0,
+                                    frame.screen_resolution.1 as i32, 
+                                    (*in_frame).data.as_mut_ptr(), 
+                                    (*in_frame).linesize.as_ptr());
+                },
+                _ => {
+                    sys::avpicture_free(in_frame as *mut sys::AVPicture);
+                    return Err(EncodeError::Format);
+                }
+            }
+
+            let pkt: *mut sys::AVPacket = std::ptr::null_mut();
+            sys::av_init_packet(pkt);
+            // (*pkt).data = std::ptr::null_mut();    // packet data will be allocated by the encoder
+            (*pkt).size = 0;
+
+            let mut got_output: i32 = 0;
+            
+            let r = sys::avcodec_encode_video2(self.codec_context, pkt, in_frame, &mut got_output);
+
+            if r >= 0 && got_output > 0
+            {
+                let encoded_frame = EncodedFrame {
+                    size: 0,
+                    magic: 0,
+                    version: 1,
+                    capture_time: frame.time,
+                    encoded_time: 0, // TODO
+                    codec: Codec::H264X264,
+                    width: frame.resolution.0 as u16,
+                    height: frame.resolution.1 as u16, 
+                    data: Vec::from_raw_parts(  (*pkt).data, 
+                                                (*pkt).size as usize,
+                                                (*pkt).size as usize),
+                };
+                
+                sys::av_packet_unref(pkt);
+
+                return Ok(encoded_frame);
+            }
+            else
+            {
+                return Err(EncodeError::Encode);
+            }
+        }
     }
 }
 
