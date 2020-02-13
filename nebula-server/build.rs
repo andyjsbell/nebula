5a8ec245c0ca120e56e84e1bc45f63a3b1a3b582
Andy Bell <andy.bell@displaynote.com>
2020-02-13 11:29:24 +0100

    parametise build, lookup VS installation

--- nebula-server/build.rs
+++ nebula-server/build.rs
@@ -5,16 +5,28 @@ use std::path::Path;
 
 fn main() {
     
-    // // Tell cargo to tell rustc to link the system bzip2
-    // // shared library.
-    // println!("cargo:rustc-link-lib=avcodec");
+    let tool = cc::windows_registry::find_tool("msvc", "devenv").unwrap();
+    let p = tool.path().parent().unwrap().parent().unwrap().parent().unwrap();
+    let mut b = p.to_path_buf();
+    b.push("VC");
+    b.push("Tools");
+    b.push("MSVC");
+    b.push("14.16.27023");
+    b.push("atlmfc");
+    
+    // Tell cargo to tell rustc to link the system bzip2
+    // shared library.
+    println!("cargo:rustc-link-lib=avcodec");
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=src/*.h");
     println!("cargo:rerun-if-changed=src/*.cpp");
 
-    // Link path and libs - TODO need to parametise this
-    println!(r"cargo:rustc-link-search=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Tools\MSVC\14.16.27023\atlmfc\lib\x64");
+    // Link path and libs
+    print!("cargo:rustc-link-search=");
+    print!("{}", b.to_str().unwrap());
+    println!("\\lib\\x64");
+    
     println!(r"cargo:rustc-link-search=C:\Users\andyb\Downloads\ffmpeg-4.2.2-win64-dev\lib");
     println!(r"cargo:rustc-link-lib=D3D11");
     println!(r"cargo:rustc-link-lib=dxgi");
@@ -23,8 +35,10 @@ fn main() {
     println!(r"cargo:rustc-link-lib=mfplat");
     println!(r"cargo:rustc-link-lib=mfuuid");
     
-    // Include path - TODO need to parametise this
-    let atl_path = Path::new(r"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Tools\MSVC\14.16.27023\atlmfc\include");
+    // Include path
+    b.push("include");
+    
+    let atl_path = Path::new(b.to_str().unwrap());
     
     // Build Nebula
     cc::Build::new()
