006c5cf10ae47615c8c2da0f71a8928348c9a1ab
Andy Bell <andy.bell@displaynote.com>
2020-02-13 16:06:55 +0100

    Add websocket server code to wasm

--- nebula-wasm/src/lib.rs
+++ nebula-wasm/src/lib.rs
@@ -1,7 +1,40 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate wasm_bindgen;
+use tungstenite::{connect, Message};
+use url::Url;
+use std::time::{Instant};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn start() {
+    let (mut socket, _) =
+        connect(Url::parse("ws://localhost:9001/socket").unwrap()).expect("Can't connect");
+    
+    let mut now = Instant::now();
+    let mut count = 0;
+    loop {
+        // println!("request frame");
+        socket
+            .write_message(Message::Binary(vec!['f' as u8]))
+            .unwrap();
+        
+        let _ = socket.read_message().expect("Error reading message");
+        // println!("frame received");
+        count = count + 1;
+        if count == 100 {
+            let frame_ms = now.elapsed().as_millis() / 100;
+            println!("fps:{}", 1000 / frame_ms);
+            now = Instant::now();
+            count = 0;
+        } 
     }
 }
