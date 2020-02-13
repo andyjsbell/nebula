935c80ebbac51e2a952c08b9530ef12e5d9c7c7a
Andy Bell <andy.bell@displaynote.com>
2020-02-13 14:57:38 +0100

    time fps coming across web socket

--- nebula-client/src/main.rs
+++ nebula-client/src/main.rs
@@ -1,18 +1,27 @@
 use tungstenite::{connect, Message};
 use url::Url;
-
+use std::time::{Instant};
 fn main() {
 
-    let (mut socket, response) =
+    let (mut socket, _) =
         connect(Url::parse("ws://localhost:9001/socket").unwrap()).expect("Can't connect");
     
+    let mut now = Instant::now();
+    let mut count = 0;
     loop {
-        println!("request frame");
+        // println!("request frame");
         socket
             .write_message(Message::Binary(vec!['f' as u8]))
             .unwrap();
         
-        let msg = socket.read_message().expect("Error reading message");
-        println!("frame received {:?}", msg)
+        let _ = socket.read_message().expect("Error reading message");
+        // println!("frame received");
+        count = count + 1;
+        if count == 100 {
+            let frame_ms = now.elapsed().as_millis() / 100;
+            println!("fps:{}", 1000 / frame_ms);
+            now = Instant::now();
+            count = 0;
+        } 
     }
 }
