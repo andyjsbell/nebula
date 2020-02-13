dab189a38f3a12cd6fc1de2ca4eb1b469119a695
Andy Bell <andy.bell@displaynote.com>
2020-02-13 11:30:50 +0100

    unused imports

--- nebula-server/src/encoder.rs
+++ nebula-server/src/encoder.rs
@@ -7,31 +7,10 @@ use super::grabber::Frame;
 
 use ffmpeg4_ffi::sys::{
     self,
-    // AVFrame,
     AVDictionary,
     AVCodec,
     AVCodecContext,
     SwsContext,
-    // AVStream,
-    // AVPacket,
-    // AVFormatContext,
-    // AVOutputFormat,
-    // AVCodecParameters,
-    // AVCodecParserContext,
-    // AVMediaType,
-    // AVMediaType_AVMEDIA_TYPE_UNKNOWN as AVMEDIA_TYPE_UNKNOWN,
-    // AVMediaType_AVMEDIA_TYPE_VIDEO as AVMEDIA_TYPE_VIDEO,
-    // AVMediaType_AVMEDIA_TYPE_AUDIO as AVMEDIA_TYPE_AUDIO,
-    // AVMediaType_AVMEDIA_TYPE_DATA as AVMEDIA_TYPE_DATA,
-    // AVMediaType_AVMEDIA_TYPE_SUBTITLE as AVMEDIA_TYPE_SUBTITLE,
-    // AVMediaType_AVMEDIA_TYPE_ATTACHMENT as AVMEDIA_TYPE_ATTACHMENT,
-    // AVMediaType_AVMEDIA_TYPE_NB as AVMEDIA_TYPE_NB,
-    // AVFMT_NOFILE,
-    // AVIO_FLAG_WRITE,
-    // AVRounding_AV_ROUND_NEAR_INF as AV_ROUND_NEAR_INF,
-    // AVRounding_AV_ROUND_PASS_MINMAX as AV_ROUND_PASS_MINMAX,
-    // AVCodecID_AV_CODEC_ID_H264 as AV_CODEC_ID_H264,
-    // AV_INPUT_BUFFER_PADDING_SIZE,
 };
 
 fn number_of_threads(width: u32, height: u32, number_of_cores:u8) -> i32 {
