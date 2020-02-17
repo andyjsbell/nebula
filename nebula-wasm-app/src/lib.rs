6febb6429ec510ed122d93ea19e7c6f0c75a552e
Andrew Bell <andy.bell@displaynote.com>
2020-02-17 16:18:07 +0100

    start of media extension implementation

--- nebula-wasm-app/src/lib.rs
+++ nebula-wasm-app/src/lib.rs
@@ -2,7 +2,7 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{ErrorEvent, MessageEvent, WebSocket, Blob};
+use web_sys::{ErrorEvent, Event, EventTarget, SourceBuffer, MessageEvent, WebSocket, Blob, MediaSource, Url};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -20,7 +20,57 @@ extern "C" {
     fn log(s: &str);
 }
 
+#[derive(Debug)]
+pub struct Player {
+    pub media_source: MediaSource,
+}
+
+impl Player {
+
+    // pub fn new() -> Player {
+        
+    // }
+}
+
 #[wasm_bindgen(start)]
+pub fn run() -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    
+    let sourceopen_callback = Closure::wrap(Box::new(move |e: Event| {
+
+        console_log!("source open");
+
+        let mime = "video/mp4; codecs=\"avc1.42E01E, mp4a.40.2\"";
+        let et = e.target().unwrap();
+        let media_source = JsCast::unchecked_ref::<MediaSource>(&et);
+        let source_buffer = media_source.add_source_buffer(mime).unwrap();
+        // get buffer from socket, wait...
+        // TODO 
+        // append buffer
+
+        // Check updating
+        let updating = source_buffer.updating();
+
+    }) as Box<dyn FnMut(Event)>);
+
+    match document.query_selector("video").unwrap() {
+        Some(video) => {
+            let media_source = MediaSource::new().unwrap();
+            let video_src = Url::create_object_url_with_source(&media_source).unwrap();
+            video.set_attribute("src", &video_src).unwrap();
+            // console_log!("{:?}", );
+            media_source.set_onsourceopen(Some(sourceopen_callback.as_ref().unchecked_ref()));
+        }
+        None => {
+            console_log!("No video element present");
+        }
+    }
+    
+    sourceopen_callback.forget();
+    Ok(())
+}
+
 pub fn start_websocket() -> Result<(), JsValue> {
     // Connect to an nebula server
     let ws = WebSocket::new("ws://localhost:9001/socket")?;
