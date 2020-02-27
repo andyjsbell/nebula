dd40726a3a29acfee5a231068c8f2bc1c59581cf
Andy Bell <andy.bell@displaynote.com>
2020-02-27 15:16:01 +0100

    Add parse function and test, albeit it fails...

--- nebula-wasm-app/src/h264.rs
+++ nebula-wasm-app/src/h264.rs
@@ -29,14 +29,54 @@ impl Nalu {
     pub fn get_size(&self ) -> u32 {
         return 4 + self.payload.len() as u32;
     }
-
 }
 
 pub fn parse_to_nalu(data: Vec<u8>) -> Vec<Nalu> {
-    let nalus : Vec<Nalu> = Vec::new();
 
-    // for x in data.step_by(4) {
-    //     println!("{}", x);
-    // }
+    let mut nalus : Vec<Nalu> = Vec::new();
+    
+    let mut append_nalu = |start_index: usize, v: &Vec<u8>, idx: usize, size: usize| {
+        let end_index = if idx < v.len() { idx } else { v.len() - 1 };
+        println!("append_nalu: {} {} {}", start_index as u32, idx as u32, end_index as u32);
+        let unit = &v[start_index + size..end_index];
+        let mut v : Vec<u8> = Vec::new();
+        v.extend_from_slice(unit);
+        let nalu = Nalu::new(v);
+        println!("nal added: {:?}", nalu.payload);
+        nalus.push(nalu);
+    };
+   
+    let mut start_index : i32 = -1;
+   
+    for n in 0..data.len() {
+        // take 4 bytes from this index and if 0x0000001 then we have a NAL
+        let window = if (n + 4) > data.len() {data.len() - 4} else {n + 4};
+        println!("window: {}", window);
+        let s = &data[n..window];
+        println!("{:?}", s);
+        match s {
+            [_,0,0,1] => {
+                println!("nal code: {}", start_index);
+                if start_index != -1 {
+                    append_nalu(start_index as usize, &data, n, 4);
+                } 
+                start_index = n as i32;
+            },
+            _ => (),
+        }
+    }
+
     nalus
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_to_nalu() {
+        let v : Vec<u8> = vec![0,0,0,1,1,2,3,4,0,0,0,1,5,6,0,0,0,1,7];
+        parse_to_nalu(v);
+        assert_eq!(3, 3);
+    }
 }
