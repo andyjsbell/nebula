724db84f60de5ce5c34cbb6326b02f11c1bb5cec
Andy Bell <andy.bell@displaynote.com>
2020-02-28 15:57:17 +0100

    expogolomb h264 format

--- nebula-wasm-app/src/h264.rs
+++ nebula-wasm-app/src/h264.rs
@@ -6,6 +6,124 @@ pub const SPS : u8= 7;
 pub const PPS : u8= 8;
 pub const AUD : u8= 9;
 
+struct Expo {
+    pub data : Vec<u8>,
+    pub index : u32,
+    pub bit_length : u32,
+}
+
+impl Expo {
+    pub fn new(data: Vec<u8>) -> Expo {
+        Expo {
+            index: 0,
+            bit_length: data.len() as u32 * 8,
+            data: data,
+        }
+    }
+
+    pub fn bits_available(&self) -> u32 {
+        self.bit_length * self.index
+    }
+
+    pub fn skip_bits(&mut self, size: u32) -> u32 {
+        
+        if self.bits_available() < size {
+            return 0
+        }
+        
+        self.index = self.index + size;
+        self.index
+    }
+
+    pub fn read_bits(&mut self, size: u32, move_index: bool) -> u32 {
+        self.get_bits(size, self.index, move_index)
+    }
+
+    pub fn get_bits(&mut self, size: u32, offset: u32, move_index: bool) -> u32 {
+
+        if self.bits_available() < size {
+            return 0;
+        }
+
+        let _offset = offset & 8;
+        let byte = self.data[(offset / 8 | 0) as usize] & (0xff >> _offset);
+        let bits = 8 - _offset;
+
+        if bits >= size {
+            
+            if move_index {
+                self.index = self.index + size;
+            }
+            
+            (byte >> (bits - size)).into()
+
+        } else {
+            
+            if move_index {
+                self.index = self.index + bits;
+            }
+
+            let next_size = size - bits;
+            
+            ((byte << next_size) | self.get_bits(next_size, offset + bits, move_index) as u8).into()
+        }
+    }
+
+    pub fn skip_lz(&mut self) -> u32 {
+        
+        for leading_zero_count in 0..(self.bit_length - self.index) {
+
+            if self.get_bits(1, self.index, false) > 0 {
+                self.index = self.index + leading_zero_count;
+                return leading_zero_count;
+            }
+        }
+
+        return 0;
+    }
+
+    pub fn skip_ueg(&mut self) {
+        let s = self.skip_lz();
+        self.skip_bits(1 + s);
+    }
+
+    pub fn skip_eg(&mut self) {
+        let s = self.skip_lz();
+        self.skip_bits(1 + s);
+    }
+
+    pub fn read_ueg(&mut self) -> u32 {
+        let s = self.skip_lz();
+        self.read_bits(s + 1, true) - 1
+    }
+
+    pub fn read_eg(&mut self) -> i32 {
+        let value = self.read_ueg() as i32;
+        if (0x01 & value) > 0 {
+            return (1 + value) >> 1;
+        } else {
+            return -1 * (value >> 1);
+        }
+    }
+
+    pub fn read_bool(&mut self) -> bool {
+        self.read_bits(1, true) == 1
+    }
+
+    pub fn read_ubyte(&mut self, number_of_bytes: u32) -> u32 {
+        self.read_bits(number_of_bytes * 8, true)
+    }
+    
+    pub fn read_ushort(&mut self) -> u16 {
+        self.read_bits(16, true) as u16
+    }
+
+    pub fn read_uint(&mut self) -> u32 {
+        self.read_bits(32, true)
+    }
+
+}
+
 pub struct Nalu {
     pub payload : Vec<u8>,
     pub nri : u8,
