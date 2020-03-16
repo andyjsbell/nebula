04e4650f978990fddb8010f66fb3ed6a7f4a3d58
Andy Bell <andy.bell@displaynote.com>
2020-03-16 10:09:29 +0100

    create media source in JS and pass to wasm

--- nebula-wasm-webpack/js/index.js
+++ nebula-wasm-webpack/js/index.js
@@ -1,1 +1,17 @@
-import("../pkg/index.js").catch(console.error);
+// function onSourceClose(_) {
+//     console.log('media source closed');
+// }
+// function onSourceOpen(_) {
+//     console.log('media source opened');
+// }
+
+// mediaSource.addEventListener('sourceclose', onSourceClose);
+// mediaSource.addEventListener('sourceopen', onSourceOpen);
+import("../pkg/index.js").then((wasm) => {
+    let mediaSource = new MediaSource();
+    var video = document.querySelector('video');
+    video.src = URL.createObjectURL(mediaSource);
+    console.log('starting...');
+    wasm.app(mediaSource);
+}).catch(console.error);
+
